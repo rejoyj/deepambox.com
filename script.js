559d4686749ce59541286c1321ab425b8faa6a72
Rejoy J <rrejoy7@gmail.com>
2024-11-27 23:10:23 +0530

    up6

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ $(document).ready(function () {
   $(".menu-items a").click(function () {
     $("#checkbox").prop("checked", false);
   });
+
+
+  document.getElementById('loadMoreBtn').addEventListener('click', () => {
+    const partnersContainer = document.querySelector('.partners-container');
+
+    const newPartnerCard = document.createElement('div');
+    newPartnerCard.classList.add('partner-card');
+    newPartnerCard.innerHTML = `
+        <img src="partner4-logo.jpg" alt="Partner 4">
+        <h3>Partner 4</h3>
+        <p>Curabitur vel bibendum eros, a condimentum lorem.</p>
+    `;
+    
+    partnersContainer.appendChild(newPartnerCard);
+});
